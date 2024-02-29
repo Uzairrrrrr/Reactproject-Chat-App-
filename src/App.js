76e21f836bc072adcff9a38138f9dd4a6c70ba10
uzair <uzairawan156@gmail.com>
2024-02-29 16:34:50 +0500

    setup the react app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import { ChatEngine } from 'react-chat-engine';}
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ChatEngine
+        height="100vh"
+        projectID="558d7603-6f30-4dec-b148-43306d687193"
+        userName=""
+        userSecret=""
+      />
     </div>
   );
 }
